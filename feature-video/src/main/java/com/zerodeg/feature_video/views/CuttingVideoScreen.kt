20f07500599c6582bd03c9bf335a8a6889d11820
youngdo <zerodeg.date@gmail.com>
2023-11-28 22:34:41 +0900

    비디오 잘라내기를 위한 드래그 UI 구현

--- feature-video/src/main/java/com/zerodeg/feature_video/views/CuttingVideoScreen.kt
+++ feature-video/src/main/java/com/zerodeg/feature_video/views/CuttingVideoScreen.kt
@@ -2,14 +2,10 @@ package com.zerodeg.feature_video.views
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -21,7 +17,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.zerodeg.domain.video_editor.BoxState
 
@@ -30,39 +28,46 @@ fun AdjustableWidthBox() {
 
     BoxWithConstraints {
 
-        val maxWidth = constraints.maxWidth // 최대 가로 길이
-        val boxState = remember { BoxState(50f, 200f, maxWidth) } // 초기 start, end 값
+        val maxWidth = constraints.maxWidth.dp // 최대 가로 길이
+        val boxState = remember { BoxState(50.dp, 200.dp, maxWidth) } // 초기 start, end 값
 
-        Box(
+        BoxWithConstraints(
             modifier = Modifier
-                .padding(start = 20.dp, end = 20.dp)
-                .width(maxWidth.dp)
+                .padding(start = 20.dp, end = 20.dp, top = 20.dp)
+                .width(maxWidth)
                 .height(150.dp)
                 .background(Color.LightGray)
         ) {
 
+            val endPos = (constraints.maxWidth.dp.value / LocalDensity.current.density).dp
+
             Box(
                 modifier = Modifier
-                    .width((boxState.end - boxState.start).dp)
-                    .offset(x = boxState.start.dp)
+                    .width((boxState.end - boxState.start))
+                    .offset(x = boxState.start)
                     .fillMaxHeight()
                     .background(Color.Yellow)
             ) {
+
+                val dragBarWidth = 20.dp
+
                 Box(
                     modifier = Modifier
                         .fillMaxHeight()
-                        .size(20.dp, 0.dp)
+                        .size(dragBarWidth, 0.dp)
                         .background(Color.Blue)
                         .align(Alignment.CenterStart)
                         .pointerInput(Unit) {
                             detectDragGestures { change, dragAmount ->
                                 change.consume()
-                                var newStart = boxState.start + dragAmount.x
-                                if (newStart < 0) newStart = 0f
-                                else if(newStart >= (boxState.end.dp.value - 20.dp.value))
-                                    newStart = (boxState.end.dp.value - 20.dp.value)
+                                var newStart = boxState.start + dragAmount.x.toDp()
+                                val endBar = (boxState.end.value - dragBarWidth.value * 2).dp
+
+                                if (newStart < 0.dp) newStart = 0.dp
+                                else if (newStart >= endBar)
+                                    newStart = endBar
                                 boxState.start = newStart
-                                Log.d("CHANGE", "${boxState.start} $density")
+                                Log.d("CHANGE", "start -> ${boxState.start}")
                             }
                         }
                 )
@@ -76,20 +81,28 @@ fun AdjustableWidthBox() {
                         .pointerInput(Unit) {
                             detectDragGestures { change, dragAmount ->
                                 change.consume()
-                                var newEnd = boxState.end + dragAmount.x
-                                val startBar = (boxState.start + 20.dp.value)
+                                var newEnd = boxState.end + dragAmount.x.toDp()
+                                val startBar = (boxState.start + (dragBarWidth.value * 2).dp)
+                                val endOfView = (endPos)
 
                                 if (newEnd <= startBar)
                                     newEnd = startBar
+                                else if (newEnd >= endOfView) newEnd = endOfView
 
                                 boxState.end = newEnd
-                                Log.d("CHANGE", "${boxState.start} $density")
+                                Log.d("CHANGE", "start ${boxState.start} end -> ${boxState.end} $endOfView")
                             }
                         }
                 )
             }
         }
     }
+}
+
+@Composable
+fun Float.toDp(): Dp {
+    val density = LocalDensity.current
+    return with(density) { this@toDp.toDp() }
 }
 
 @Preview
